30e681e0aa4b3dae302c226dd9e1e898fca546ca
ElliotBrouillard <2162349@cegepoutaouais.qc.ca>
2022-05-04 08:14:26 -0400

    Commit classe véhicule 1

---
+++ Véhicule.py
@@ -0,0 +1,8 @@
+# déclaration de la classe
+class Véhicule:
+    def __init__(self, p_roues=0, p_immatriculation=, p_annee_fabrication=0, p_prix=0):
+        self.roues=p_roues
+
+
+    def Démarrer():
+        print("Le véhicule démarre")
